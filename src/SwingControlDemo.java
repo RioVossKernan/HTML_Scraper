c047a22aa0c678e2282130dcae98803875988ff5
Rio Voss-Kernan <rio_voss-kernan23@milton.edu>
2022-11-09 12:11:29 -0500

    got visuals for HTML scrapper

--- src/SwingControlDemo.java
+++ src/SwingControlDemo.java
@@ -1,18 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 public class SwingControlDemo implements ActionListener {
     private JFrame mainFrame;
-    private JLabel headerLabel;
-    private JLabel statusLabel;
-    private JPanel controlPanel;
+
+    private JLabel urlLabel;
+    private JLabel termLabel;
+
+    private JPanel topPanel;
+    private JPanel urlPanel;
+    private JPanel termPanel;
     private JPanel textPanel;
-    private JMenuBar mb;
-    private JMenu file, edit, help;
-    private JMenuItem cut, copy, paste, selectAll;
+
+    private JButton goButton;
+
     private JTextArea ta;
+    private JTextField url;
+    private JTextField term;
+
     private int WIDTH=800;
     private int HEIGHT=700;
 
@@ -29,109 +35,87 @@ public class SwingControlDemo implements ActionListener {
         mainFrame = new JFrame("Java SWING Examples");
         mainFrame.setSize(WIDTH, HEIGHT);
         mainFrame.setLayout(new BorderLayout());
-
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
 
-        //menu
-        mb = new JMenuBar();
-        file = new JMenu("File");
-        edit = new JMenu("Edit");
-        help = new JMenu("Help");
+        //Instantiate Panels
+        textPanel = new JPanel();
+        topPanel = new JPanel();
+        urlPanel = new JPanel();
+        termPanel = new JPanel();
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(2,1));
 
-        //menu items
-        cut = new JMenuItem("cut");
-        copy = new JMenuItem("copy");
-        paste = new JMenuItem("paste");
-        selectAll = new JMenuItem("selectAll");
-        cut.addActionListener(this);
-        copy.addActionListener(this);
-        paste.addActionListener(this);
-        selectAll.addActionListener(this);
-        edit.add(cut);
-        edit.add(copy);
-        edit.add(paste);
-        edit.add(selectAll);
-
-        //add items to menu
-        mb.add(file);
-        mb.add(edit);
-        mb.add(help);
 
         //text area
-        textPanel = new JPanel();
         textPanel.setLayout(new GridLayout(1,1));
         ta = new JTextArea();
-       // ta.setBounds(10,50,WIDTH-20, HEIGHT-100);
+        ta.setBounds(200,200,50,50);
         textPanel.add(ta);
 
-
-        //set label
-        headerLabel = new JLabel("", JLabel.CENTER);
-        statusLabel = new JLabel("", JLabel.CENTER);
-        statusLabel.setSize(350, 100);
-
-        controlPanel = new JPanel();
-        controlPanel.setLayout(new GridLayout(3,1));
+        //button
+        goButton = new JButton("GO");
+        goButton.setFont(new Font("Poppins",Font.BOLD,20));
+        goButton.setPreferredSize(new Dimension(200,50));
+        goButton.setActionCommand("GO");
+        goButton.addActionListener(new ButtonClickListener());
 
 
+        //set labels
+        termLabel = new JLabel("URL:  ", JLabel.CENTER);
+        urlLabel = new JLabel("TERM:", JLabel.CENTER);
+        termLabel.setFont(new Font("Poppins",Font.BOLD,20));
+        urlLabel.setFont(new Font("Poppins",Font.BOLD,20));
 
-       // set Frame layout
-        mainFrame.add(mb);
-        mainFrame.setJMenuBar(mb);
-        mainFrame.add(statusLabel,BorderLayout.NORTH);
+        //set URL panel
+        urlPanel.setLayout(new FlowLayout());
+        urlPanel.add(urlLabel);
+        url = new JTextField();
+        url.setPreferredSize(new Dimension(400,20));
+        urlPanel.add(url);
+        inputPanel.add(urlPanel);
+
+        //set term panel
+        termPanel.setLayout(new FlowLayout());
+        termPanel.add(termLabel);
+        term = new JTextField();
+        term.setPreferredSize(new Dimension(400,20));
+        termPanel.add(term);
+        inputPanel.add(termPanel);
+
+
+        //set topPanel Layout
+        topPanel.setLayout(new FlowLayout());
+        topPanel.add(inputPanel);
+        topPanel.add(goButton);
+
+
+        // set Frame layout
         mainFrame.add(textPanel,BorderLayout.CENTER);
-        mainFrame.add(controlPanel,BorderLayout.WEST);
+        mainFrame.add(topPanel,BorderLayout.NORTH);
+
+
         mainFrame.setVisible(true);
     }
 
     private void showEventDemo() {
-        headerLabel.setText("Control in action: Button");
 
-        JButton okButton = new JButton("OK");
-        JButton submitButton = new JButton("Submit");
-        JButton cancelButton = new JButton("Cancel");
-
-        okButton.setActionCommand("OK");
-        submitButton.setActionCommand("Submit");
-        cancelButton.setActionCommand("Cancel");
-
-        okButton.addActionListener(new ButtonClickListener());
-        submitButton.addActionListener(new ButtonClickListener());
-        cancelButton.addActionListener(new ButtonClickListener());
-
-        controlPanel.add(okButton);
-        controlPanel.add(submitButton);
-        controlPanel.add(cancelButton);
-
-        mainFrame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == cut)
-            ta.cut();
-        if (e.getSource() == paste)
-            ta.paste();
-        if (e.getSource() == copy)
-            ta.copy();
-        if (e.getSource() == selectAll)
-            ta.selectAll();
+
     }
 
     private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
 
-            if (command.equals("OK")) {
-                statusLabel.setText("Ok Button clicked.");
-            } else if (command.equals("Submit")) {
-                statusLabel.setText("Submit Button clicked.");
-            } else {
-                statusLabel.setText("Cancel Button clicked.");
+            if (command.equals("GO")) {
+
             }
         }
     }
